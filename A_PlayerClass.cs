654d53dbe4c84fbf6d6fa322713f37bb241b81f7
Riley <23508141+Barney@users.noreply.github.com>
2020-08-17 01:07:13 -0400

    Update A_PlayerClass.cs
    Removed old unused functionality

--- A_PlayerClass.cs
+++ A_PlayerClass.cs
@@ -2,12 +2,11 @@ using System;
 
 public class A_PlayerClass
 {
-	public A_PlayerClass(string name, int hp = 0, int armour = 0, string lastHitBy = "Nobody", int userID = -1)
+	public A_PlayerClass(string name, int hp, int armour, int userID)
 	{
 		this.name = name;
 		this.hp = hp;
 		this.armour = armour;
-		this.lastHitBy = lastHitBy;
 		this.userID = userID;
 	}
 
@@ -16,8 +15,6 @@ public class A_PlayerClass
 	public int hp;
 
 	public int armour;
-
-	public string lastHitBy;
 
 	public int userID;
 }
